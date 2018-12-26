9e824c9e7c15b711a389cb13b5c2d1a1f8ee3c31
Osman Bineev <bineev@gmail.com>
2018-12-27 00:59:12 +0300

    now fqr is just a Faqtor CLI

---
+++ fqr.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+if (process.argv.length < 3) {
+    console.error("target name expected");
+    process.exit(1);
+}
+
+const fs = require("fs");
+const resolve = require('path').resolve;
+
+const fileExists = async (name) => {
+    return new Promise((resolve) => {
+        fs.access(name, (err) => resolve(err === null));
+    });
+};
+
+async function runFactor(name, x, argv) {
+    if (typeof x === "function") {
+        return await x(argv);
+    }
+    if (x.run && typeof x.run === "function") {
+        return await x.run(argv);
+    }
+    return new Error(`FQR: something wrong with factor "${name}"`);
+}
+
+
+(async () => {
+    const v1 = "./build/fqr.config.js";
+    const v2 = "./fqr.config.js";
+    const argv = process.argv.slice(2);
+
+    for (const v of [v1, v2]) {
+        if (await fileExists(v)) {
+            try {
+                const rv = resolve(v);
+                const tab = require(rv);
+                const name = argv[0];
+                const err = await runFactor(name, tab[name], argv.slice(1));
+                if (err) {
+                    console.error(err);
+                    process.exit(1);
+                }
+                process.exit(0);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }
+    console.error("No 'fqr.config.js' found");
+    process.exit(1);
+})();
---
+++ test/build/fqr.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    hello: async () => {
+        console.log("Hello World");
+        return null;
+    },
+    helloFactor: {
+        run: async () => {
+            console.log("Hello World from Factor");
+            return null;
+        }
+    }
+}
